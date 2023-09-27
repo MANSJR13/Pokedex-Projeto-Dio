c6b985a4dbacb8b2b37c9db9320da494c1a21121
Marco Aurelio Junior <m.aureliosilvajr@outlook.com.br>
2023-09-27 17:39:12 -0300

    escrevendo js e separando

---
+++ assets/js/dinamic.js
@@ -0,0 +1,30 @@
+
+ 
+function convertPokemonToLi(pokemon) {
+    return `
+        <li class="pokemon">
+            <span class="number">#001</span>
+            <span class="name">${pokemon.name}</span>
+
+            <div class="detail">
+                <ol class="types">
+                    <li class="type">Gross</li>
+                    <li class="type">Poison</li>
+                </ol>
+                    
+                <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png" 
+                    alt="${pokemon.name}">
+            </div>
+        </li>
+    `
+}
+
+const pokemonList = document.getElementById ('pokemonList')
+
+pokeApi.getPokemons().then((pokemons)=> {
+        for (let i = 0; i < pokemons.length; i++) {
+            const pokemon = pokemons[i];
+            pokemonList.innerHTML += convertPokemonToLi(pokemon)      
+        }
+    })  
+    .catch((error) => console.error(error))
