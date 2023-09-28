4772141c4a3103bcf461e112504912312fb82047
Marco Aurelio Junior <m.aureliosilvajr@outlook.com.br>
2023-09-28 04:08:53 -0300

    adições ao JS

--- assets/js/dinamic.js
+++ assets/js/dinamic.js
@@ -1,26 +1,28 @@
-
+function convertPokemonTypesToLi(pokemonTypes) {
+    return pokemonTypes.map((typeSlot) => `<li class="type">${typeSlot.type.name}</li>`)
+}
  
 function convertPokemonToLi(pokemon) {
     return `
         <li class="pokemon">
-            <span class="number">#001</span>
+            <span class="number">${pokemon.order}</span>
             <span class="name">${pokemon.name}</span>
 
             <div class="detail">
                 <ol class="types">
-                    <li class="type">Gross</li>
-                    <li class="type">Poison</li>
+                    ${convertPokemonTypesToLi(pokemon.types).join('')}
                 </ol>
                     
-                <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png" 
+                <img src"${pokemon.sprites.other.dream_world.front_default}" 
                     alt="${pokemon.name}">
             </div>
         </li>
     `
 }
 
-const pokemonList = document.getElementById ('pokemonList')
+const pokemonList = document.getElementById('pokemonList')
 
 pokeApi.getPokemons().then((pokemons = [])=> {
-    pokemonList.innerHTML += pokemons.map(convertPokemonToLi).join('')   
+    const newHtml = pokemons.map(convertPokemonToLi).join('')
+    pokemonList.innerHTML = newHtml  
 })  
